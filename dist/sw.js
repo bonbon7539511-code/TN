cd53f5f0449b2ea424869442c40f89a3aa05185e
DESKTOP-9IDMKM2\User <aaa>
2026-09-21 17:01:55 +0800

    Build travel planning workspace

---
+++ dist/sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'trip-notes-v1';
+self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/', '/index.html', '/manifest.webmanifest', '/favicon.svg']))));
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
