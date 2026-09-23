a822d9f99fb6c3ce3a7e6251e7fa4b4943bc09e2
DESKTOP-9IDMKM2\User <aaa>
2026-09-23 10:55:39 +0800

    Polish trip list controls

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'trip-notes-v4';
+const CACHE = 'trip-notes-v5';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', (event) => event.waitUntil(
